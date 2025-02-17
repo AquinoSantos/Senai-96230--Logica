5b04a2f219b248bba4e5e0874af0d57d452383eb
AquinoSantos <aquinisantiago@gmail.com>
2025-02-17 18:42:07 +0000

    Salvando

---
+++ 2. tipagem_estatica.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("clear")
+# Definição de tipos obrigatória.
+
+nome: str # Equivalente ao tipo Caracter
+idade: int # Equivalente ao tipo Inteiro
+aluno: float # Equivalente ao tipo Real
+
+nome= "Marta"
+idade: 23
+altura: 1.65
+
+print(nome)
+print(idade)
+print(altura)
---
+++ 3. tipagem.dinamica.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("clear")
+# Definição de tipos obrigatória.
+
+
+
+nome= "Marta"
+idade: 23
+altura: 1.65
+
+print(nome)
+print(idade)
+print(altura)
